f46e9904f9af34d83a79840efee4035cb33dc6b5
Felix Rabe <felix@rabe.io>
2014-10-03 17:23:05 +0200

    Building Ubuntu is currently broken
    See http://redmine.named-data.net/issues/2013#note-9

--- build-all.sh
+++ build-all.sh
@@ -7,6 +7,6 @@ CMD() {
   "$@"
 }
 
-CMD docker build -t named_data/ubuntu-14.04  ubuntu-14.04
+# CMD docker build -t named_data/ubuntu-14.04  ubuntu-14.04
 CMD docker build -t named_data/debian-jessie debian-jessie
 CMD docker build -t named_data/node-0.10     node-0.10
